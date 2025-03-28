916243a80dd33382b344ea2a3f533ea03c6a06ce
Eric Parache <aceric715@gmail.com>
2025-03-28 12:58:45 -0400

    fix tailwind issue

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -8,14 +8,14 @@ export default function Footer() {
         <footer className="footer bg-slate-800 dark:bg-gray-900 relative text-gray-200 dark:text-gray-200">
             <div className="py-[30px] px-0 border-t border-slate-800">
                 <div className="container relative text-center">
-                    <div className="grid lg:grid-cols-12 md:grid-cols-3 grid-cols-1 items-center">
+                    <div className="grid lg:grid-cols-14 md:grid-cols-3 grid-cols-1 items-center">
                         <div className="lg:col-span-2 md:text-start text-center">
                             <Link to="#" className="text-[22px] focus:outline-none">
                                 <img src={logo} className="mx-auto md:me-auto md:ms-0" alt="" />
                             </Link>
                         </div>
 
-                        <div className="lg:col-span-5 text-center mt-6 md:mt-0">
+                        <div className="lg:col-span-6 text-center mt-6 md:mt-0">
                             <p className="mb-0">© {new Date().getFullYear()} Comb Inc. Design & Develop with <i className="mdi mdi-heart text-red-600"></i> by <a href="https://shreethemes.in/" target="_blank" className="text-reset">Comb Devs</a>.</p>
                         </div>
 
@@ -26,7 +26,7 @@ export default function Footer() {
                             </form>
                         </div>
 
-                        <ul className="lg:col-span-2 list-none md:text-end text-center mt-6 md:mt-0">
+                        <ul className="lg:col-span-1 list-none md:text-end text-center mt-6 md:mt-0">
                             <li className="inline mx-[2px]"><a href="tel:+15133995139" target="_blank" className="size-8 inline-flex justify-center items-center border border-gray-700 dark:border-gray-800 rounded-md hover:border-teal-500 dark:hover:border-teal-500 hover:bg-teal-500 dark:hover:bg-teal-500"><FiPhoneCall className="size-4 align-middle" title="phone" /></a></li>
                             <li className="inline mx-[2px]"><a href="mailto:support@triocomb.com" className="size-8 inline-flex justify-center items-center border border-gray-700 dark:border-gray-800 rounded-md hover:border-teal-500 dark:hover:border-teal-500 hover:bg-teal-500 dark:hover:bg-teal-500"><FiMail className="size-4 align-middle" title="email"></FiMail></a></li>
                         </ul>
